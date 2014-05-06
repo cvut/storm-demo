22ce94365fb1fb35810404731ee4bad411e801b3
Tomas Barton <barton.tomas@gmail.com>
2014-05-06 11:47:32 +0200

    word count example

---
+++ 01-word-count/src/main/java/storm/word/count/RandomSentenceSpout.java
@@ -0,0 +1,52 @@
+package storm.word.count;
+
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichSpout;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Values;
+import backtype.storm.utils.Utils;
+
+import java.util.Map;
+import java.util.Random;
+
+public class RandomSentenceSpout extends BaseRichSpout {
+
+    SpoutOutputCollector _collector;
+    Random _rand;
+
+    private static final String[] sentences = new String[]{
+        "one king took the fox over the car.",
+        "two queens bent the fox under the bed.",
+        "four bears mined the pig into the house.",
+        "Joe goats rolled the boat over the lodge.",};
+
+    @Override
+    public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
+        _collector = collector;
+        _rand = new Random();
+    }
+
+    @Override
+    public void nextTuple() {
+        Utils.sleep(100);
+
+        String sentence = sentences[_rand.nextInt(sentences.length)];
+        _collector.emit(new Values(sentence));
+    }
+
+    @Override
+    public void ack(Object id) {
+    }
+
+    @Override
+    public void fail(Object id) {
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields("word"));
+    }
+
+}
