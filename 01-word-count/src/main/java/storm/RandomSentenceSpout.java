49348ce25d94e596dc7b28218046e4d49ac5b412
Tomas Barton <barton.tomas@gmail.com>
2014-05-06 12:06:05 +0200

    changed package name

--- 01-word-count/src/main/java/storm/RandomSentenceSpout.java
+++ 01-word-count/src/main/java/storm/RandomSentenceSpout.java
@@ -1,4 +1,4 @@
-package storm.word.count;
+package storm;
 
 import backtype.storm.spout.SpoutOutputCollector;
 import backtype.storm.task.TopologyContext;
--- 01-word-count/src/main/java/storm/WordCountTopology.java
+++ 01-word-count/src/main/java/storm/WordCountTopology.java
@@ -1,4 +1,4 @@
-package storm.word.count;
+package storm;
 
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
